de8d35dd5b021829cacdd64554c6fd3b72c12d1f
anthonyvigz <anthonyvigz@gmail.com>
2019-06-09 20:21:52 -0400

    added list properties

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -3,8 +3,11 @@ import React from 'react';
 
 const Character = props => {
     return (
+            <div>
             <div className="characters" onClick={props.homeworld}>
                 {props.charitem.name}
+            </div>
+            <h1 className="mass">Mass: {props.charitem.mass}</h1>
             </div>
     )
 }
